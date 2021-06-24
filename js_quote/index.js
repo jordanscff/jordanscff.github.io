794d33a118932752c825bd91bc8d388291b1f7d6
jordanscff <54541513+jordanscff@users.noreply.github.com>
2021-06-24 20:25:25 +0100

    Add files via upload

---
+++ js_quote/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//Get Quote
+let quotes = document.getElementById('quotes')
+quotes.addEventListener("click", evt => {
+  let quotesDiv = document.getElementById('quotes-div')
+  fetch('https://api.kanye.rest')
+  .then(res => res.json())
+  .then(quote => {
+      quotesDiv.innerHTML =  `<p> ${quote.quote} </p>`
+  })
+})
